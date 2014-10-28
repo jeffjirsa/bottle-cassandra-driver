0c04a364d6e09a92bbef5f7683774735a396eff4
Jeff Jirsa <jjirsa@badjuju.com>
2014-10-28 13:21:39 -0700

    Address issue #1 - cassandra on different port - by exposing Cluster() constructor options

--- bottle_cassandra_driver.py
+++ bottle_cassandra_driver.py
@@ -19,9 +19,45 @@ class CassandraPlugin(object):
         """Return the cassandra connection from the environment."""
         return self.connection
 
-    def __init__(self, endpoints=['127.0.0.1'], keyspace=None, consistency_level=cassandra.ConsistencyLevel.QUORUM,
-            keyword='cqlconnection', **kwargs):
-        self.cluster =  Cluster(endpoints)
+    def __init__(self, endpoints=['127.0.0.1'], 
+            keyspace=None, 
+            consistency_level=cassandra.ConsistencyLevel.QUORUM,
+            keyword='cqlconnection', 
+            port=9042,
+            compression=True,
+            auth_provider=None,
+            load_balancing_policy=None,
+            reconnection_policy=None,
+            default_retry_policy=None,
+            conviction_policy_factory=None,
+            metrics_enabled=False,
+            connection_class=None,
+            ssl_options=None,
+            sockopts=None,
+            cql_version=None,
+            protocol_version=2,
+            executor_threads=2,
+            max_schema_agreement_wait=10,
+            control_connection_timeout=2.0,
+            **kwargs):
+        self.cluster =  Cluster(endpoints, 
+                                port=port, 
+                                compression=compression,
+                                auth_provider=auth_provider,
+                                load_balancing_policy=load_balancing_policy,
+                                reconnection_policy=reconnection_policy,
+                                default_retry_policy=default_retry_policy,
+                                conviction_policy_factory=conviction_policy_factory,
+                                metrics_enabled=metrics_enabled,
+                                connection_class=connection_class,
+                                ssl_options=ssl_options,
+                                sockopts=sockopts,
+                                cql_version=cql_version,
+                                protocol_version=protocol_version,
+                                executor_threads=executor_threads,
+                                max_schema_agreement_wait=max_schema_agreement_wait,
+                                control_connection_timeout=control_connection_timeout
+                                )
         self.keyspace = keyspace
         self.connection = self.cluster.connect(self.keyspace)
         self.consistency_level = consistency_level
